dba98b53bd58370d6b02004434c7cfe459ea1e7b
Bill La Forge <laforge49@gmail.com>
2014-08-13 14:43:36 -0400

    fixed isolation transitive but but unable to port the test for same.

--- jactor2-coreSt/src/main/java/org/agilewiki/jactor2/core/impl/stRequests/RequestStImpl.java
+++ jactor2-coreSt/src/main/java/org/agilewiki/jactor2/core/impl/stRequests/RequestStImpl.java
@@ -201,14 +201,21 @@ public abstract class RequestStImpl<RESPONSE_TYPE> implements
     public void doSend(final ReactorImpl _source,
             final AsyncResponseProcessor<RESPONSE_TYPE> _responseProcessor) {
         final ReactorStImpl source = (ReactorStImpl) _source;
+        use();
+        responseProcessor = _responseProcessor;
+        if (responseProcessor == null) {
+            responseProcessor = (AsyncResponseProcessor<RESPONSE_TYPE>) OneWayResponseProcessor.SINGLETON;
+        }
+        requestSource = source;
         if (!source.isRunning()) {
             throw new IllegalStateException(
                     "A valid source sourceReactor can not be idle");
         }
+        oldMessage = source.getCurrentRequest();
         if ((oldMessage != null) && oldMessage.isIsolated()) {
             isolated = true;
-        }
-        isolated = !source.isCommonReactor();
+        } else
+            isolated = !source.isCommonReactor();
         if (!(targetReactor instanceof CommonReactor)) {
             if (isolated && (_responseProcessor != null)) {
                 throw new UnsupportedOperationException(
@@ -216,16 +223,8 @@ public abstract class RequestStImpl<RESPONSE_TYPE> implements
             }
             isolated = true;
         }
-        use();
-        AsyncResponseProcessor<RESPONSE_TYPE> rp = _responseProcessor;
-        if (rp == null) {
-            rp = (AsyncResponseProcessor<RESPONSE_TYPE>) OneWayResponseProcessor.SINGLETON;
-        }
-        requestSource = source;
-        oldMessage = source.getCurrentRequest();
         sourceExceptionHandler = (ExceptionHandler<RESPONSE_TYPE>) source
                 .getExceptionHandler();
-        responseProcessor = rp;
         final boolean local = targetReactor == source.asReactor();
         targetReactorImpl.unbufferedAddMessage(this, local);
     }
