a8835899a7f96f4511be83cc9bda3ad7da2c004f
Bill La Forge <laforge49@gmail.com>
2014-11-30 19:54:55 +0530

    added visited optimization

--- jactor2-coreSt/src/main/java/org/agilewiki/jactor2/core/impl/stReactors/IsolationReactorStImpl.java
+++ jactor2-coreSt/src/main/java/org/agilewiki/jactor2/core/impl/stReactors/IsolationReactorStImpl.java
@@ -1,13 +1,12 @@
 package org.agilewiki.jactor2.core.impl.stReactors;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.agilewiki.jactor2.core.reactors.IsolationReactor;
 import org.agilewiki.jactor2.core.reactors.NonBlockingReactor;
 import org.agilewiki.jactor2.core.reactors.impl.ReactorImpl;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Internal implementation of UnboundReactor.
@@ -19,7 +18,7 @@ public class IsolationReactorStImpl extends PoolThreadReactorStImpl {
     /**
      * Create an IsolationReactorMtImpl.
      *
-     * @param _parentReactor         The parent reactor.
+     * @param _parentReactor The parent reactor.
      */
     public IsolationReactorStImpl(final NonBlockingReactor _parentReactor) {
         super(_parentReactor);
@@ -55,13 +54,17 @@ public class IsolationReactorStImpl extends PoolThreadReactorStImpl {
         if (resources.containsKey(_reactorImpl))
             return true;
         Set<IsolationReactorStImpl> rs = new HashSet<IsolationReactorStImpl>(resources.size());
+        Set<IsolationReactorStImpl> visited = new HashSet<IsolationReactorStImpl>(resources.size());
         while (rs.size() > 0) {
             IsolationReactorStImpl i = rs.iterator().next();
-            if (i.resources.containsKey(_reactorImpl)) {
-                resources.put((IsolationReactorStImpl) _reactorImpl, Boolean.TRUE);
-                return true;
+            if (!visited.contains(i)) {
+                if (i.resources.containsKey(_reactorImpl)) {
+                    resources.put((IsolationReactorStImpl) _reactorImpl, Boolean.TRUE);
+                    return true;
+                }
+                rs.addAll(i.resources.keySet());
+                visited.add(i);
             }
-            rs.addAll(i.resources.keySet());
             rs.remove(i);
         }
         return false;
